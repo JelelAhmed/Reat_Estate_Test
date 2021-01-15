aa14b942370562911eb2c514702ae32e25851d8c
jelel6 <abduljelelahmed@gmail.com>
2021-01-14 19:36:03 -0800

    Updates

---
+++ precache-manifest.d1f76d4597f68e4cc59a66ae06f85062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6509895e5338d2fead14",
+    "url": "/Reat_Estate_Test/static/js/runtime~main.b6319270.js"
+  },
+  {
+    "revision": "373c26da7e94a6db6a31",
+    "url": "/Reat_Estate_Test/static/js/main.d0361c4c.chunk.js"
+  },
+  {
+    "revision": "d19a8679478165635dd7",
+    "url": "/Reat_Estate_Test/static/js/2.4ed93c14.chunk.js"
+  },
+  {
+    "revision": "373c26da7e94a6db6a31",
+    "url": "/Reat_Estate_Test/static/css/main.d69569b8.chunk.css"
+  },
+  {
+    "revision": "d19a8679478165635dd7",
+    "url": "/Reat_Estate_Test/static/css/2.f730bfe1.chunk.css"
+  },
+  {
+    "revision": "4afda97af6377f723eb0520d5e73670b",
+    "url": "/Reat_Estate_Test/index.html"
+  }
+];
